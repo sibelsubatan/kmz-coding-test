f815c1c8a9f45a1ab090ffa1fe6ed59fedc69579
sibelsubatan <sibelsubatan@gmail.com>
2023-11-15 22:21:44 +0300

    commit

--- ios/akilliTicaretCaseTests/akilliTicaretCaseTests.m
+++ ios/akilliTicaretCaseTests/akilliTicaretCaseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface kmzcodingtestTests : XCTestCase
+@interface akilliTicaretCaseTests : XCTestCase
 
 @end
 
-@implementation kmzcodingtestTests
+@implementation akilliTicaretCaseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
